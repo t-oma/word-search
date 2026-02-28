9b716a26cb8dd46b5a80a75bb084f022414ac7d0
Artem Levchenko <levchencode@gmail.com>
2026-03-01 01:03:14 +0200

    try prerender pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  basename: process.env.BASENAME_URL!,
+  basename: "/",
   ssr: false,
   appDirectory: "./src/app",
+  prerender: true,
 } satisfies Config;
