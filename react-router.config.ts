00877b91428222c2a763eb972e8e4b76d2987949
Artem Levchenko <levchencode@gmail.com>
2026-02-28 23:56:08 +0200

    prerender pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import { games } from "./src/entities/game/model/games";
 
 const basename = process.env.BASENAME_URL!;
+
+const gameIDs = games.map((g) => g.id);
 
 export default {
   // Config options...
@@ -8,4 +11,5 @@ export default {
   basename,
   ssr: false,
   appDirectory: "./src/app",
+  prerender: ["/", "/privacy-policy", "/stats", ...gameIDs.map((id) => `/games/${id}`)],
 } satisfies Config;
