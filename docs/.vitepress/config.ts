da0a5e113da341eb09ad4c39a5ecc9644bd013ae
Artem Levchenko <levchencode@gmail.com>
2026-02-28 16:44:21 +0200

    deploy vitepress docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/word-search/docs/",
   title: "Word Search",
   description:
     "A word search puzzle game built with React Router, TypeScript, and Tailwind CSS.",
