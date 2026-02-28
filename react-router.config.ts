194bfef969c883e3dde217bceb519a6c1109d185
Artem Levchenko <levchencode@gmail.com>
2026-02-28 16:27:34 +0200

    fix: react router basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  basename: "/word-search/",
   ssr: false,
   appDirectory: "./src/app",
 } satisfies Config;
