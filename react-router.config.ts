727bf616b4865c944c7fa5f6992c6851d834b39c
Artem Levchenko <levchencode@gmail.com>
2026-02-28 22:54:51 +0200

    fix: basename error

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "@react-router/dev/config";
 
+const basename = process.env.BASENAME_URL!;
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  basename,
   ssr: false,
   appDirectory: "./src/app",
 } satisfies Config;
